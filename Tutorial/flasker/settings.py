b4d354d760ac12c72d7ea5ddca31354fce3b70b0
Harry Ji <xiangyu.ji@outlook.com>
2015-06-15 17:31:33 +0800

    finish Flasker; Notive: enviromental variable

---
+++ Tutorial/flasker/settings.py
@@ -0,0 +1,10 @@
+# configuration
+import sqlite3
+
+FLASKR_SETTINGS={
+	"DATABASE" : '/tmp/flaskr.db',
+	"DEBUG" : True,
+	"SECRET_KEY" : 'development key',
+	"USERNAME" : 'admin',
+	"PASSWORD" : 'default',
+}
